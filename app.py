11462cf697bc84222b5caed9cc421e2c7bed8ba7
Keifrick Bateman <akulistus555@gmail.com>
2023-09-22 23:09:03 +0300

    changed output

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-    name = request.args.get("name")
-    message = request.args.get("message")
-    return f'''<p> Hello {name}! <p> 
-                <p>{message} <p>'''
+    name = request.args.get("name", default='НЕ УКАЗАНО', type=str)
+    message = request.args.get("message", default='НЕ УКАЗАНО', type=str)
+    return f'''<h1> Hello {name}! <h1> 
+                <h1>{message} <h1>'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port =80)
