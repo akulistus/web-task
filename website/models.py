bf5d511177827cc8551ed33ed08332c2bab25d06
Keifrick Bateman <akulistus555@gmail.com>
2023-10-05 01:48:09 +0300

    implemented crud

--- website/models.py
+++ website/models.py
@@ -3,12 +3,13 @@ from . import db
 from sqlalchemy import Integer, String, Column, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
-appointments = db.Table(
-    'appointments',
-    Column('doctor_id', ForeignKey('doctors.id'), primary_key=True),
-    Column('patient_id', ForeignKey('patients.id'), primary_key=True),
-    Column('Info')
-)
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+    doctor_id: Mapped[int] = mapped_column(Integer, ForeignKey('doctors.id'), primary_key=True)
+    patient_id: Mapped[int] = mapped_column(Integer, ForeignKey('patients.id'), primary_key=True)
+    info: Mapped[str] = mapped_column(String, nullable=True)
+    doctor: Mapped["Doctor"] = db.relationship(back_populates="patients")
+    patient: Mapped["Patient"] = db.relationship(back_populates="doctors")
 
 class Hospital(db.Model):
     __tablename__ = 'hospitals'
@@ -29,7 +30,7 @@ class Doctor(db.Model, UserMixin):
     surname: Mapped[str] = mapped_column(String, nullable=False)
     password: Mapped[int] = mapped_column(Integer, nullable=False)
     hospital_id: Mapped[int] = mapped_column(Integer, ForeignKey(Hospital.id))
-    patients = db.relationship('Patient', secondary = appointments, backref = 'doctors')
+    patients: Mapped[list["Appointment"]] = db.relationship(back_populates='doctor')
 
 class Patient(db.Model, UserMixin):
     __tablename__ = 'patients'
@@ -38,4 +39,5 @@ class Patient(db.Model, UserMixin):
     surname: Mapped[str] = mapped_column(String, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
     passport_id: Mapped[int] = mapped_column(Integer, ForeignKey(Passport.id))
+    doctors: Mapped[list["Appointment"]] = db.relationship(back_populates='patient')
 
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, request
 from . import db
-from .models import Doctor, Hospital
+from .models import Doctor, Hospital, Appointment
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -27,7 +27,28 @@ def get_doctors(id):
 @login_required
 def create_appointment(id):
     doctor = Doctor.query.get(id)
-    user = current_user
-    doctor.patients.append(user)
+    appointment = Appointment()
+    appointment.patient = current_user
+    appointment.doctor = doctor
+    doctor.patients.append(appointment)
     db.session.commit()
-    return jsonify({})
+    return home()
+
+@views.route('/delete-appointment/<did>,<pid>')
+@login_required
+def delete_appointment(did,pid):
+    appointmen = Appointment.query.get({"doctor_id": did, "patient_id": pid})
+    db.session.delete(appointmen)
+    db.session.commit()
+    return appointments()
+
+@views.route('/edit-appointment/<did>,<pid>', methods = ['GET', 'POST'])
+@login_required
+def edit_appointment(did, pid):
+    if request.method == 'POST':
+        appointmen = Appointment.query.get({"doctor_id": did, "patient_id": pid})
+        info = request.form.get('info')
+        appointmen.info = info
+        db.session.commit()
+
+    return appointments()
